657c39c4abe0f8aa48a47c63761648fc45fd8ad0
Ulrich Pogson <grapplerulrich@gmail.com>
2013-01-05 17:16:00 +0100

    Fix for Firefox

--- js/responsive-opt-out.js
+++ js/responsive-opt-out.js
@@ -1,38 +1,55 @@
 // Copyright (c) 2013 Ulrich Pogson, https://github.com/grapplerulrich/responsive-opt-out.js
 
-if (cookie('responsive_opt_out') == 'fixed width') {
-    function FixedWidth() {
-        (function(body, classToAdd) {
-            body.className = (body.className).replace("responsive", classToAdd);
-        })(document.body, "fixed-width");
+// check if the there is a cookie with the value 'fixed width'
+if (cookie( 'responsive_opt_out' ) == 'fixed width' ) {
 
-        viewport = document.querySelector("meta[name=viewport]");
-        viewport.setAttribute('content', 'width=device-width, user-scalable=yes, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0');
+	// replace body class responsive with fixed-width
+	function FixedWidth() {
+		(function(body, classToAdd) {
+			body.className = (body.className).replace("responsive", classToAdd);
+		})(document.body, "fixed-width");
 
-        if (cookie.enabled()) {
-            cookie.set('responsive_opt_out', 'fixed width', {
-                path: "/"
-            });
-        } else {
-            // Cookies are turned off you can not use cookies
-        }
-    }
-    window.onload = FixedWidth;
+		// change meta viewport
+		viewport = document.querySelector("meta[name=viewport]");
+		viewport.setAttribute('content', 'width=device-width, user-scalable=yes, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0');
+	}
+
+	// Load function
+	window.onload = FixedWidth;
 }
 
+// load function onClick
+function FixedWidth() {
+	(function(body, classToAdd) {
+		body.className = (body.className).replace("responsive", classToAdd);
+	})(document.body, "fixed-width");
+
+	// change meta viewport
+	viewport = document.querySelector("meta[name=viewport]");
+	viewport.setAttribute('content', 'width=device-width, user-scalable=yes, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0');
+
+	// if cookies are enabled then set a cookie with a value 'fixed width'
+	if (cookie.enabled()) {
+		cookie.set( 'responsive_opt_out', 'fixed width' );
+	} else {
+		// Cookies are turned off you can not use cookies
+	}
+}
+
+// load function onClick
 function Responsive() {
-    (function(body, classToAdd) {
-        body.className = (body.className).replace("fixed-width", classToAdd);
-    })(document.body, "responsive");
+	(function(body, classToAdd) {
+		body.className = (body.className).replace("fixed-width", classToAdd);
+	})(document.body, "responsive");
 
-    viewport = document.querySelector("meta[name=viewport]");
-    viewport.setAttribute('content', 'width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0');
+	// change meta viewport
+	viewport = document.querySelector("meta[name=viewport]");
+	viewport.setAttribute('content', 'width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0');
 
-    if (cookie.enabled()) {
-        cookie.set('responsive_opt_out', 'responsive', {
-            path: "/"
-        });
-    } else {
-        // Cookies are turned off you can not use cookies
-    }
+	// if cookies are enabled then set a cookie with a value 'responsive'
+	if (cookie.enabled()) {
+		cookie.set( 'responsive_opt_out', 'responsive' );
+	} else {
+		// Cookies are turned off you can not use cookies
+	}
 }
